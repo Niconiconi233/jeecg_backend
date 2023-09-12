853f247262c805812134ff05a701ef8f3a84f68d
yw <1660994874@qq.com>
2023-09-12 23:25:13 +0800

    添加列表数据权限

--- nkhg-management/src/main/java/org/yw/nkhg/rulemanagement/controller/RuleLibController.java
+++ nkhg-management/src/main/java/org/yw/nkhg/rulemanagement/controller/RuleLibController.java
@@ -1,41 +1,26 @@
 package org.yw.nkhg.rulemanagement.controller;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.jeecg.common.api.vo.Result;
-import org.jeecg.common.system.query.QueryGenerator;
-import org.jeecg.common.util.oConvertUtils;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import lombok.extern.slf4j.Slf4j;
-
-import org.jeecgframework.poi.excel.ExcelImportUtil;
-import org.jeecgframework.poi.excel.def.NormalExcelConstants;
-import org.jeecgframework.poi.excel.entity.ExportParams;
-import org.jeecgframework.poi.excel.entity.ImportParams;
-import org.jeecgframework.poi.excel.view.JeecgEntityExcelView;
-import org.jeecg.common.system.base.controller.JeecgController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
-import com.alibaba.fastjson.JSON;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.jeecg.common.aspect.annotation.AutoLog;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.jeecg.common.api.vo.Result;
+import org.jeecg.common.aspect.annotation.AutoLog;
+import org.jeecg.common.aspect.annotation.PermissionData;
+import org.jeecg.common.system.base.controller.JeecgController;
+import org.jeecg.common.system.query.QueryGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.yw.nkhg.rulemanagement.entity.RuleLib;
 import org.yw.nkhg.rulemanagement.service.IRuleLibService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 /**
  * @Description: 规章制度管理模块
@@ -63,6 +48,7 @@ public class RuleLibController extends JeecgController<RuleLib, IRuleLibService>
 	//@AutoLog(value = "规章制度管理模块-分页列表查询")
 	@ApiOperation(value="规章制度管理模块-分页列表查询", notes="规章制度管理模块-分页列表查询")
 	@GetMapping(value = "/list")
+	@PermissionData(pageComponent = "nkhg/rulemanagement/RuleLibList")
 	public Result<IPage<RuleLib>> queryPageList(RuleLib ruleLib,
 								   @RequestParam(name="pageNo", defaultValue="1") Integer pageNo,
 								   @RequestParam(name="pageSize", defaultValue="10") Integer pageSize,
